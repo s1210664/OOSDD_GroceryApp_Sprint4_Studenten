c8e3d5038f9a208ab25a166b0a54bb9fdc079f3b
s1210664 <s1210664@student.windesheim.nl>
2025-10-02 16:12:57 +0200

    werkende UC 11 en begin met UC 13

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -3,12 +3,19 @@ namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
+        public enum Privilege
+        {
+            None,
+            Admin
+        }
+        public Privilege Role { get; set; }
         public string EmailAddress { get; set; }
         public string Password { get; set; }
-        public Client(int id, string name, string emailAddress, string password) : base(id, name)
+        public Client(int id, string name, string emailAddress, string password, Privilege role = Privilege.None) : base(id, name)
         {
             EmailAddress=emailAddress;
             Password=password;
+            Role=role;
         }
     }
 }
